579fc1bcfa09775e796c6343e4b9ce8b12d8f499
SlaviHristev <Slavihristev97@gmail.com>
2023-09-18 18:59:08 +0300

    Added scoring system

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const choicesDiv = document.getElementById('choices');
 let playerMove = '';
 let result = '';
 let moves = '';
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
+
 
 document.querySelector('.rock').addEventListener('click', playerMoveRock)
 function playerMoveRock() {
@@ -22,10 +27,13 @@ function playerMoveRock() {
     playerMove = "Rock";
     if (computerChoice === "Paper") {
         result = "Computer Wins!!!";
+        computerScore++;
     } else if (computerChoice === "Scissors") {
         result = "Player Wins!!!";
+        playerScore++;
     } else {
         result = "It's a Draw!!!";
+        draws++;
     };
     moves = `Player chooses ${playerMove}, Computer chooses ${computerChoice}`;
     let newPElement = document.createElement('p');
@@ -34,6 +42,12 @@ function playerMoveRock() {
     resultPElement.textContent = `${result}`
     choicesDiv.appendChild(newPElement);
     choicesDiv.appendChild(resultPElement);
+
+    document.getElementById('player-score').textContent = `Player Score = ${playerScore}`;
+    document.getElementById('computer-score').textContent = `Computer Score = ${computerScore}`;
+    document.getElementById('draws-score').textContent = `Draws = ${draws}`
+
+
 };
 
 document.querySelector('.paper').addEventListener('click', playerMovePaper)
@@ -41,13 +55,16 @@ function playerMovePaper() {
     computerChoice = computerMove();
     choicesDiv.innerHTML = '';
     playerMove = "Paper";
-    
+
     if (computerChoice === "Paper") {
         result = "It's a Draw!!!";
+        draws++;
     } else if (computerChoice === "Scissors") {
         result = "Computer Wins!!!";
+        computerScore++;
     } else {
         result = "Player Wins!!!";
+        playerScore++;
     };
     moves = `Player chooses ${playerMove}, Computer chooses ${computerChoice}`;
     let newPElement = document.createElement('p');
@@ -56,6 +73,12 @@ function playerMovePaper() {
     resultPElement.textContent = `${result}`
     choicesDiv.appendChild(newPElement);
     choicesDiv.appendChild(resultPElement);
+
+    document.getElementById('player-score').textContent = `Player Score = ${playerScore}`;
+    document.getElementById('computer-score').textContent = `Computer Score = ${computerScore}`;
+    document.getElementById('draws-score').textContent = `Draws = ${draws}`
+
+
 };
 
 document.querySelector('.scissors').addEventListener('click', playerMoveScissors)
@@ -65,10 +88,13 @@ function playerMoveScissors() {
     playerMove = "Scissors";
     if (computerChoice === "Paper") {
         result = "Player Wins!!!";
+        playerScore++;
     } else if (computerChoice === "Scissors") {
         result = "It's a Draw!!!";
+        draws++;
     } else {
         result = "Computer Wins!!!";
+        computerScore++;
     };
     moves = `Player chooses ${playerMove}, Computer chooses ${computerChoice}`;
     let newPElement = document.createElement('p');
@@ -77,6 +103,9 @@ function playerMoveScissors() {
     resultPElement.textContent = `${result}`
     choicesDiv.appendChild(newPElement);
     choicesDiv.appendChild(resultPElement);
+    document.getElementById('player-score').textContent = `Player Score = ${playerScore}`;
+    document.getElementById('computer-score').textContent = `Computer Score = ${computerScore}`;
+    document.getElementById('draws-score').textContent = `Draws = ${draws}`
+
+
 };
-
-
